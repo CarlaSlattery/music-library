65c90ff2b1ca1198d857de4ed6c8675bd31e490e
CarlaSlattery <cslatterydev@gmail.com>
2022-12-17 12:16:31 +0000

    set up create artist test - passing

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const artistRouter = require('./routes/artist');
 
 const app = express();
 app.use(express.json());
+app.use('/artist', artistRouter);
 
 app.get('/', (__req, res) => {
   return res.status(200).json({ result: 'Hello World' });
