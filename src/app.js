b297293b941336236c867a7097fe9b09be1ebcde
CarlaSlattery <cslatterydev@gmail.com>
2022-12-19 16:22:44 +0000

    added security to create artist

--- src/app.js
+++ src/app.js
@@ -5,8 +5,4 @@ const app = express();
 app.use(express.json());
 app.use('/artists', artistRouter);
 
-app.get('/', (__req, res) => {
-  return res.status(200).json({ result: 'Hello World' });
-});
-
 module.exports = app;
